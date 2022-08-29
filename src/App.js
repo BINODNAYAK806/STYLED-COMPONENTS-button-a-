c9deb95a6054e9af4d6d8d21514dc22edf59118f
BINODNAYAK806 <binodnayak8066@gmail.com>
2022-08-30 03:00:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import styled, { css } from 'styled-components'
+// import styled from 'styled-components';
+
+<script src="https://unpkg.com/styled-components/dist/styled-components.min.js"></script>
+
+
 
 function App() {
+
+
+  const Button = styled.button`
+  background: blue;
+  border-radius: 3px;
+  border: 2px solid ;
+  color: white;
+  margin: 0.5em 1em;
+  padding: 0.25em 1em;
+  
+  ${props => props.link && css`
+    background: transparent;
+    color: blue;
+  `}
+  ${props => props.primary && css`
+    background: transparent;
+    color: black;
+  `}
+  ${props => props.sec && css`
+    background: transparent;
+    color: black;
+     border: 2px dashed black ;
+    
+  `}
+  ${props => props.def && css`
+    background: transparent;
+    color: black;
+     border: 2px solid black ;
+    
+  `}
+`;
+
+const Container = styled.div`
+  text-align: center;
+`
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <Container>
+        <h1>binod</h1>
+    <Button>Primary Button</Button>
+    <Button def>Default Button</Button>
+    <Button sec>sec Button</Button>
+    <Button primary>text button</Button>
+    
+    <Button link>link Button</Button>
+    
+  </Container>
+     
   );
 }
 
